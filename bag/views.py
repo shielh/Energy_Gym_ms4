90bdf31c06556f41c6ecffb83c87c5b35f88457f
shielh <shiel.helen@gmail.com>
2021-11-06 15:52:45 +0000

    Add checkout app and model

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, HttpResponse
 from django.contrib import messages
 
 from products.models import Product
@@ -24,7 +24,7 @@ def add_to_bag(request, item_id):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
-        messages.success(request, f'Added {product.name} to your bag')
+        messages.error(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
@@ -46,15 +46,23 @@ def adjust_bag(request, item_id):
 
 
 def remove_from_bag(request, item_id):
-    """Adjust the quantity of the specified product to the shopping bag"""
-    quantity = int(request.POST.get('quantity'))
-    bag = request.session.get('bag', {})
+    """Remove the item from the shopping bag"""
+    """ Update to exclude size"""
     try:
-        if quantity > 0:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        bag = request.session.get('bag', {})
+
+        if size:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+        else:
             bag.pop(item_id)
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
-        
+
     except Exception as e:
         return HttpResponse(status=500)
