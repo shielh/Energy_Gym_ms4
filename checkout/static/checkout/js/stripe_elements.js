d501518c5f6cee1d61c5c4456c0ce064594727b0
shielh <shiel.helen@gmail.com>
2021-11-10 21:29:53 +0000

    Add stripe payment field to checkout page

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,36 @@
+/* Logic for this is from:
+https://stripe.com/docs/payments/accept-a-payment
+
+CSS from:
+https://stripe.com/docs/js/appendix/style
+*/
+
+var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+var client_secret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripe_public_key);
+var elements = stripe.elements();
+var card = elements.create('card', {style: style});
+
+var style = {
+      base: {
+        iconColor: '#c4f0ff',
+        color: '#fff',
+        fontWeight: '500',
+        fontFamily: 'Roboto, Open Sans, Segoe UI, sans-serif',
+        fontSize: '16px',
+        fontSmoothing: 'antialiased',
+        ':-webkit-autofill': {
+          color: '#fce883',
+        },
+        '::placeholder': {
+          color: '#87BBFD',
+        },
+      },
+      invalid: {
+        iconColor: '#FFC7EE',
+        color: '#FFC7EE',
+      },
+  };
+
+card.mount('#card-element');
+
--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     templates = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JuNRsIN2hzrbz3FGUp72CKseJWbFuag8dkDd6tELFksAE7nFEtXxCDm2a1aRHqGwvSA8B9SXA6AraMc9MaU7jBW008vKAWtmA',
+        'client_secret': 'test client secret',
     }
 
     return render(request, templates, context)
