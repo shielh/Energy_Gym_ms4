e1c743188210113d4c37776fa15d88542016f680
shielh <shiel.helen@gmail.com>
2021-11-14 13:24:13 +0000

    Basic checkout functionality now working

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,53 +1,46 @@
-/* Logic for this is from:
-https://stripe.com/docs/payments/accept-a-payment
-
-CSS from:
-https://stripe.com/docs/js/appendix/style
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+    CSS from here: 
+    https://stripe.com/docs/stripe-js
 */
 
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
-var card = elements.create('card', {style: style});
-
 var style = {
-      base: {
-        iconColor: '#c4f0ff',
-        color: '#fff',
-        fontWeight: '500',
-        fontFamily: 'Roboto, Open Sans, Segoe UI, sans-serif',
-        fontSize: '16px',
+    base: {
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
         fontSmoothing: 'antialiased',
-        ':-webkit-autofill': {
-          color: '#fce883',
-        },
+        fontSize: '16px',
         '::placeholder': {
-          color: '#87BBFD',
-        },
-      },
-      invalid: {
-        iconColor: '#FFC7EE',
-        color: '#FFC7EE',
-      },
-  };
-
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
-// Handle the realtime validation errors on card elements
+// Handle realtime validation errors on the card element
 card.addEventListener('change', function (event) {
-  var errorDiv = document.getElementById('card-errors');
-  if (event.error) {
-      var html = `
-          <span class="icon" role="alert">
-              <i class="fas fa-times"></i>
-          </span>
-          <span>${event.error.message}</span>
-      `;
-      $(errorDiv).html(html);
-  } else {
-      errorDiv.textContent = '';
-  }
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
 });
 
 // Handle form submit
