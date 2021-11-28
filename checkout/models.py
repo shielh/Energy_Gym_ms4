c5e572b82b9f202a84a8e4c77352999d86a1eb8a
shielh <shiel.helen@gmail.com>
2021-11-28 16:51:41 +0000

    Create custom error pages

--- checkout/models.py
+++ checkout/models.py
@@ -11,7 +11,7 @@ from profiles.models import UserProfile
 class Order(models.Model):
     order_id = models.CharField(max_length=50, null=False, editable=False)
     user_id = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
-                                 null=True, blank=True, related_name='orders')
+                                null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=100, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
@@ -21,11 +21,15 @@ class Order(models.Model):
     address_line1 = models.CharField(max_length=75, null=False, blank=False)
     address_line2 = models.CharField(max_length=75, null=False, blank=False)
     date = models.DateTimeField(auto_now_add=True)
-    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
-    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    delivery_cost = models.DecimalField(
+        max_digits=6, decimal_places=2, null=False, default=0)
+    order_total = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0)
     original_bag = models.TextField(null=False, blank=False, default='')
-    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
+    stripe_pid = models.CharField(
+        max_length=254, null=False, blank=False, default='')
 
     def _generate_order_id(self):
         """ 
@@ -48,7 +52,6 @@ class Order(models.Model):
         self.grand_total = self.order_total + self.delivery_cost
         self.save()
 
-
     def save(self, *args, **kwargs):
         """
         Override the original save method to set the order id
@@ -60,11 +63,17 @@ class Order(models.Model):
 
 
 class OrderLineItem(models.Model):
-    order_lineitem_id = models.CharField(max_length=50, null=False, editable=False)
-    order_id = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
-    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    order_lineitem_id = models.CharField(max_length=50, null=False,
+                                         editable=False)
+    order_id = models.ForeignKey(Order, null=False, blank=False,
+                                 on_delete=models.CASCADE,
+                                 related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False,
+                                on_delete=models.CASCADE)
     quantity = models.IntegerField(null=False, blank=False, default=0)
-    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2,
+                                         null=False, blank=False,
+                                         editable=False)
 
     def save(self, *args, **kwargs):
         """
--- reviews/urls.py
+++ reviews/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.reviews, name='reviews'),
-    path('write_review/', views.write_review, name='write_review'),
+    path('add_review/', views.add_review, name='add_review'),
     path('edit_review/<review_id>', views.edit_review, name='edit_review'),
     path('delete_review/<review_id>', views.delete_review, name='delete_review'),
 ]
