0bbc29c5106b61647a719430d76ac6c017955d1f
shielh <shiel.helen@gmail.com>
2021-11-30 20:33:19 +0000

    Add models to contact app

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from profiles.models import UserProfile
+from checkout.models import Order
 
-# Create your models here.
+
+class Contact(models.Model):
+    user = models.ForeignKey(
+        UserProfile, related_name='contact', on_delete=models.CASCADE,
+        blank=True, null=True)
+    email = models.ForeignKey(
+        Order, related_name='contact', on_delete=models.CASCADE,
+        blank=True, null=True)
+    message = models.TextField()
+
+    def __str__(self):
+        return f'{self.user}'
