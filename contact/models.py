1989ed8ef3e53fed2feb3e7f90feccba915954db
shielh <shiel.helen@gmail.com>
2021-12-02 14:52:25 +0000

    Update requirements file

--- contact/models.py
+++ contact/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Contact(models.Model):
-    name = models.CharField(max_length=75)
-    email = models.EmailField()
-    message = models.TextField()
+    name = models.CharField(max_length=75, default='')
+    email = models.EmailField(default='')
+    message = models.TextField(default='')
 
     def __str__(self):
-        return f'{self.name}'
+        return f'{self.email}'
