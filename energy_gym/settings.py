69d2df70b4d4470aa1cd9bb5ed48a583543408ae
shielh <shiel.helen@gmail.com>
2021-12-04 16:54:00 +0000

    Add site logo to navbar

--- energy_gym/settings.py
+++ energy_gym/settings.py
@@ -26,8 +26,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
+
 
 ALLOWED_HOSTS = ['energy-gym-ms4.herokuapp.com', 'localhost']
 
