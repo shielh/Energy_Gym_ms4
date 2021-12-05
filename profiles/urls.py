beb483457869ef7cf371cede6b7abbd80bfe5ee2
shielh <shiel.helen@gmail.com>
2021-12-05 15:21:32 +0000

    Fix PEP8 errors in profiles app

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
-    path('order_history/<order_id>', views.order_history, name='order_history'),
+    path('order_history/<order_id>', views.order_history,
+         name='order_history'),
 ]
